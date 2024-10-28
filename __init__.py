932d2b6e6a978b5f2b15a1474ad132ca10f7a687
hunterssl <hunterssl@163.com>
2024-10-28 14:45:15 +0800

    init2

--- __init__.py
+++ __init__.py
@@ -1,8 +1,3 @@
-from .nodes.a1基础格式 import a1
-from .nodes.a2基础数据类型 import a2
-from .nodes.a3基础调用流程 import a3
-from .nodes.a4一个可以运行的节点 import a4
-from .nodes.a5最简格式 import a5
 from .nodes.nodes import (
     SSLLoadJson,
     SSLGetJsonKeysCount,
@@ -14,11 +9,6 @@ from .nodes.nodes import (
 # （必填）填写 import的类名称，命名需要唯一，key或value与其他插件冲突可能引用不了。这是决定是否能引用的关键。
 # key(自定义):value(import的类名称)
 NODE_CLASS_MAPPINGS = {
-    "a1": a1,
-    "a2": a2,
-    "a3": a3,
-    "a4": a4,
-    "a5": a5,
     "SSLLoadJson": SSLLoadJson,
     "SSLGetJsonKeysCount": SSLGetJsonKeysCount,
     "SSLLoadCheckpointByName": SSLLoadCheckpointByName,
@@ -30,12 +20,6 @@ NODE_CLASS_MAPPINGS = {
 # （可不写）填写 ui界面显示名称，命名会显示在节点ui左上角，如不写会用类的名称显示在节点ui上
 # key(自定义):value(ui显示的名称)
 NODE_DISPLAY_NAME_MAPPINGS = {
-    # a000_example
-    "a1": "a1基础格式~",
-    "a2": "a2基础数据类型~",
-    "a3": "a3基础调用流程~",
-    "a4": "a4一个可以运行的节点~",
-    "a5": "a5最简格式~",
     "SSLLoadJson": "SSL Load Json",
     "SSLGetJsonKeysCount": "SSL Get Json Keys Count",
     "SSLLoadCheckpointByName": "SSL Load Checkpoint By Name",
